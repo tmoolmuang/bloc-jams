245d3157514af9d80b2bb9204abffb8435b80d99
tmoolmuang <tmoolmuang@yahoo.com>
2017-01-25 19:18:37 -0800

    module2-checkpoint08-assignment: completed (fixed)

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,12 @@
-var animatePoints = function() { 
+var animatePoints = function() {
 	var points = document.getElementsByClassName('point');
-	
-	var revealPoint = function(points) {
-		for (var i=0; i<points.length; i++) {
-			points[i].style.opacity = 1;
-			points[i].style.transform = "scaleX(1.1) translateY(3.2rem)";
-			points[i].style.msTransform = "scaleX(1.1) translateY(3.2rem)";
-			points[i].style.WebkitTransform = "scaleX(1.1) translateY(3.2rem)";
-			points[i].style.color = "lightblue";
-		}
+	var revealPoint = function(index) {
+		points[index].style.opacity = 1;
+		points[index].style.transform = "scaleX(1) translateY(0)";
+		points[index].style.msTransform = "scaleX(1) translateY(0)";
+		points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+	};
+	for (var i=0; i<points.length; i++) {
+		revealPoint(i);
 	}
-	
-	revealPoint(points);
 };	
