daeca42b4e2c968444b4af13741f902cd328fd72
tmoolmuang <tmoolmuang@yahoo.com>
2017-02-02 11:47:09 -0800

    module2-checkpoint17: completed

--- scripts/collection.js
+++ scripts/collection.js
@@ -1,4 +1,5 @@
-var collectionItemTemplate =
+ var buildCollectionItemTemplate = function() {
+	 var template =
      '<div class="collection-album-container column fourth">'
    + '  <img src="assets/images/album_covers/01.png"/>'
    + '  <div class="collection-album-info caption">'
@@ -13,14 +14,20 @@ var collectionItemTemplate =
    + '  </div>'
    + '</div>'
    ;
-
- window.onload = function() {
-   // #1
-	 var collectionContainer = document.getElementsByClassName('album-covers')[0];
+ 
 	 // #2
-	 collectionContainer.innerHTML = '';
+	 return $(template);
+ };
+
+ $(window).load(function() {
+   // #3
+	 var $collectionContainer = $('.album-covers');
+	 // #4
+	 $collectionContainer.empty();
 	 // #3
 	 for (var i = 0; i < 12; i++) {
-		 collectionContainer.innerHTML += collectionItemTemplate;
+		 var $newThumbnail = buildCollectionItemTemplate();
+		 // #5
+		 $collectionContainer.append($newThumbnail);
 	 }
- }
+ });
